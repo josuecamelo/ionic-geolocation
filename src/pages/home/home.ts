1aeacdf4ca3a8aad630181b4831a30ea4e5cd33f
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2018-04-23 11:04:42 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform, NavController } from 'ionic-angular';
+
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  loc = {
+    lat: 0,
+    long: 0
+  };
 
+  constructor(public navCtrl: NavController, public platform: Platform, private geolocation: Geolocation) {
+    platform.ready().then(() => {
+      this.initMap();
+    });
   }
 
+  initMap() {
+    this.geolocation.getCurrentPosition({ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }).then((resp) => {
+        this.loc.lat = resp.coords.latitude;
+        this.loc.long = resp.coords.longitude;
+    });
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      this.loc.lat = data.coords.latitude;
+      this.loc.long = data.coords.longitude;
+    });
+  }
 }
